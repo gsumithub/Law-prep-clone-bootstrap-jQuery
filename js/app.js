594ca8014a018339ad2092caa36583977677d6b8
Sumit Kumar <115206686+gsumithub@users.noreply.github.com>
2025-12-25 14:20:12 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,38 @@
+$('#topperVideoSlider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('#gallerySlider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
